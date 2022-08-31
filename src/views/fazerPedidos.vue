3df823e600cc6a529f1eaffa6cbe9fa6af1b65fe
Rodrigo Soares <diihhsoares2@gmail.com>
2022-08-30 22:20:30 -0300

    criando componentes de pedidos

---
+++ src/views/fazerPedidos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="main-container">
+      <h1>Monte o seu Hambúrguer:</h1>
+        <BurgerForm />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import BurgerForm from '../components/BurgerForm'
+
+export default {
+  name: 'fazerPedidos',
+  components: {
+    BurgerForm
+  }
+}
+</script>
